389b89a88a1f7046539ae9d6027b033c9620249f
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-05-03 10:38:51 +0300

    Done. M1:task5-6

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home),
     path('about', views.about),
     path('item/<int:item_id>', views.get_item),
+    path('items', views.get_items),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -52,7 +52,16 @@ def get_item(request, item_id: int):
             result = f"""
             <h2> Имя: {item["name"]} </h2>
             <p> Количество: {item["quantity"]} </p>
+            <p> <a href='/items'> Назад к списку товаров </a></p>
             """
             return HttpResponse(result)
 
     return HttpResponseNotFound(f'Item with id={item_id} not found')
+
+
+def get_items(request):
+    result = "<h1> Список товаров </h1><ol>"
+    for item in items:
+        result += f""" <li><a href='/item/{item["id"]}'> {item["name"]} </a></li> """
+    result += "</ol>"
+    return HttpResponse(result)
