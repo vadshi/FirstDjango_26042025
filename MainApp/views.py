aa3d66ed38a481eba0c33b2c1d685614411230c4
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-05-10 10:30:36 +0300

    Done.M4:task1-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
-
-
-items = [
-   {"id": 1, "name": "Кроссовки abibas", "quantity":5},
-   {"id": 2, "name": "Куртка кожаная", "quantity":2},
-   {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
-   {"id": 7, "name": "Картофель фри", "quantity":0},
-   {"id": 8, "name": "Кепка", "quantity":124},
-]
 
 
 def home(request) -> HttpResponse:
@@ -37,19 +29,18 @@ def about(request):
 
 
 def get_item(request, item_id: int):
-    """ TODO: get item by id from db."""
-    for item in items:
-        if item["id"] == item_id:
-            context = {
-                "item": item
-            }
-            return render(request, "item_page.html", context)
-    return render(request, "errors.html", {'errors': [f'Item with id={item_id} not found']})
-
+    """ Get item by id from db."""
+    try:    
+        item = Item.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        return render(request, "errors.html", {'errors': [f'Item with id={item_id} not found']})
+    else:
+        context = {"item": item}
+        return render(request, "item_page.html", context)
+    
 
 def get_items(request):
-    """ TODO: get all items from db."""
-    context = {
-        "items": items 
-        }
+    """ Get all items from db."""
+    items = Item.objects.all()
+    context = {"items": items}
     return render(request, "items_list.html", context)
