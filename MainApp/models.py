dcd757277bfff68cf895228d677a62074317702b
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-05-10 14:42:01 +0300

    Add Color to Item(many-to-many relationship)

--- MainApp/models.py
+++ MainApp/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Color(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __repr__(self):
+        return f'Color({self.name})'
+
+
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField() 
     description = models.TextField(max_length=300, default="Описание элемента")
+    colors = models.ManyToManyField(to=Color)
 
